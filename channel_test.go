a480679f91f00be1618019c7e097c49665519e30
rafel-akp <rafelinoxz7@gmail.com>
2023-06-09 17:13:25 +0800

    buffer channel

--- channel_test.go
+++ channel_test.go
@@ -34,3 +34,35 @@ func TestChannelAsParameter(t *testing.T) {
 	fmt.Println(data)
 	defer close(channel)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "rafelino"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(3 * time.Second)
+	defer close(channel)
+}
+
+func TestBufferChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	channel <- "rafel"
+	channel <- "claudius"
+	channel <- "kelen"
+
+	fmt.Println(cap(channel)) //melihat panjang buffer
+	fmt.Println(len(channel)) // melihat jumlah data di dalam buffer
+}
